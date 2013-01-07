efc361f2f4800d5bbc64f1e3658a8678090d3d2c
Tobias Kirschstein <lev@ideegeo.com>
2013-01-07 01:39:50 +0100

    version bump

--- lib/Web/API.pm
+++ lib/Web/API.pm
@@ -19,11 +19,11 @@ Web::API - A Simple base module to implement almost every RESTful API with just 
 
 =head1 VERSION
 
-Version 0.3
+Version 0.4
 
 =cut
 
-our $VERSION = "0.3";
+our $VERSION = "0.4";
 
 =head1 SYNOPSIS
 
